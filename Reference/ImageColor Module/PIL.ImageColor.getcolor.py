2d61646ed5be518b03c6836aaa595b35e3ed3e30
weiguangfan <826008728@qq.com>
2022-07-20 14:42:18 +0800

    add files

---
+++ Reference/ImageColor Module/PIL.ImageColor.getcolor.py
@@ -0,0 +1,12 @@
+"""
+PIL.ImageColor.getcolor(color, mode)
+与getrgb()相同，
+但如果模式不是彩色或调色板图像，
+则将RGB值转换为灰度值。
+如果字符串不能被解析，
+这个函数会引发ValueError异常。
+
+1.1.4版中的新内容。
+
+
+"""
---
+++ Reference/ImageColor Module/PIL.ImageColor.getrgb.py
@@ -0,0 +1,10 @@
+"""
+PIL.ImageColor.getrgb(color)
+将一个颜色字符串转换为RGB元组。
+如果该字符串不能被解析，
+该函数会引发ValueError异常。
+
+在1.1.4版本中新增。
+
+
+"""
