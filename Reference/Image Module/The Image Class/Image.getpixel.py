c4d1606b5fe22638a8889fc64643dc4c7a25b441
weiguangfan <826008728@qq.com>
2022-07-20 11:27:57 +0800

    add files

---
+++ Reference/Image Module/The Image Class/Image.getpixel.py
@@ -0,0 +1,12 @@
+"""
+Image.getpixel(xy)
+返回指定位置的像素值。
+
+PARAMETERS:
+    xy - 坐标，以（x，y）的形式给出。
+        参见Coordinate System.
+
+RETURNS:
+    像素值。
+    如果图像是一个多层图像，该方法返回一个元组。
+"""
---
+++ Reference/Image Module/The Image Class/Image.getprojection.py
@@ -0,0 +1,7 @@
+"""
+Image.getprojection()
+获取对x轴和y轴的投影
+
+RETURNS:
+    两个序列，分别表示沿X轴和Y轴有非零像素的地方。
+"""
